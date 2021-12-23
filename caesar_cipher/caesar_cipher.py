765a9754b4b74affca71179e13230c7d379031a9
Joe Streifel <josephstreifel@gmail.com>
2021-12-22 19:32:15 -0800

    completes encrypt function

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,10 +1,20 @@
 def encrypt(text, shift_num):
+    encoded_string = ""
+
     for char in text:
         char_num = ord(char)
-        if 65 <= char <= 90:
-            pass
+
+        if 65 <= char_num <= 90:
+            origin = 65
         elif 97 <= char_num <=122:
-            pass
+            origin = 97
+        
+        shifted = ((char_num - origin) + shift_num) % 26
+        char = chr(shifted + origin)
+
+        encoded_string += char
+    print(text, shift_num, encoded_string)
+    return encoded_string
 
 def decrypt(encoded, shift_num):
     pass
