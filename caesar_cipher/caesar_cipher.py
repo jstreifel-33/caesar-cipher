e96a65198b065c5c45ac594ae77f27cd9f032a05
Joe Streifel <josephstreifel@gmail.com>
2021-12-22 17:04:35 -0800

    brings in tests, begins encrypt function

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,13 @@
+def encrypt(text, shift_num):
+    for char in text:
+        char_num = ord(char)
+        if 65 <= char <= 90:
+            pass
+        elif 97 <= char_num <=122:
+            pass
+
+def decrypt(encoded, shift_num):
+    pass
+
+def crack(encoded):
+    pass
